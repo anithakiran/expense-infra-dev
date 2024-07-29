1c1e36738d04d0d25be952a5785988d184d4605c
anithakiran <anitha1995d@gmail.com>
2024-07-29 10:28:41 +0530

    expense-infra-dev

---
+++ 04-openvpn/main.tf
@@ -0,0 +1,18 @@
+module "vpn" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "${var.project_name}-${var.environment}-vpn"
+
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  subnet_id  = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+  
+
+  tags = merge (
+      var.common_tags, 
+      {
+        Name = "${var.project_name}-${var.environment}-openvpn"
+      }
+  )
+}
