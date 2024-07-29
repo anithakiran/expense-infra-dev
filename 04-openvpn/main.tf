7d4d9f172cc6a125e77a3077d67c76577530c84e
anithakiran <anitha1995d@gmail.com>
2024-07-29 11:10:02 +0530

    expense-infra-dev

--- 04-openvpn/main.tf
+++ 04-openvpn/main.tf
@@ -1,18 +1,22 @@
+resource "aws_key_pair" "vpn" {
+  key_name   = "openvpn"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEucv5PXUGqg0oD90OLvajvppgiwipNDUDlzcj3IdfoE ANITHA@LAPTOP-ABTH6DBN"
+  #public_key = file("D:/Applications/openvpn.pub")
+}
+
+
 module "vpn" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-
+  key_name = aws_key_pair.vpn.key_name
   name = "${var.project_name}-${var.environment}-vpn"
-
   instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
   subnet_id  = local.public_subnet_id
   ami = data.aws_ami.ami_info.id
-  
-
   tags = merge (
       var.common_tags, 
       {
-        Name = "${var.project_name}-${var.environment}-openvpn"
+        Name = "${var.project_name}-${var.environment}-vpn"
       }
   )
 }
